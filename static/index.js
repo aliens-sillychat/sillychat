7f43f76f7328a6c187e2c16472afff7b16f3154e
solo5star <solo_5@naver.com>
2022-11-17 06:23:43 +0900

    prettier 포맷팅 실행

--- static/index.js
+++ static/index.js
@@ -17,111 +17,113 @@ const WS_ENDPOINT = 'ws://sillychat.kro.kr/api/ws';
 let ws = null;
 
 function uuidv4() {
-    return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
-        (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
-    );
+  return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, (c) =>
+    (c ^ (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))).toString(16)
+  );
 }
 
 function createElement(html) {
-    const template = document.createElement('template');
-    html = html.trim(); // Never return a text node of whitespace as the result
-    template.innerHTML = html;
-    return template.content.firstChild;
+  const template = document.createElement('template');
+  html = html.trim(); // Never return a text node of whitespace as the result
+  template.innerHTML = html;
+  return template.content.firstChild;
 }
 
 /**
- * @param {string} username 
+ * @param {string} username
  */
-function setUser(username = null){
-    const user = getUser();
+function setUser(username = null) {
+  const user = getUser();
 
-    localStorage.setItem('user', JSON.stringify({
-        id: user?.id ?? uuidv4(),
-        name: username,
-    }));
+  localStorage.setItem(
+    'user',
+    JSON.stringify({
+      id: user?.id ?? uuidv4(),
+      name: username,
+    })
+  );
 }
 
 /**
  * @returns {User}
  */
 function getUser() {
-    const serializedUser = localStorage.getItem('user');
-    if (serializedUser === null) return null;
-    return JSON.parse(serializedUser);
+  const serializedUser = localStorage.getItem('user');
+  if (serializedUser === null) return null;
+  return JSON.parse(serializedUser);
 }
 
 function resetUser() {
-    localStorage.removeItem('user');
-    setUser();
-    return getUser();
+  localStorage.removeItem('user');
+  setUser();
+  return getUser();
 }
 
-
 /**
- * @param {string} message 
+ * @param {string} message
  */
 function sendMessage(content) {
-    const message = {
-        type: 'message',
-        user: getUser(),
-        content,
-    };
-    ws?.send(JSON.stringify(message));
+  const message = {
+    type: 'message',
+    user: getUser(),
+    content,
+  };
+  ws?.send(JSON.stringify(message));
 }
 
 function sendJoin() {
-    const message = {
-        type: 'join',
-        user: getUser(),
-    };
-    ws?.send(JSON.stringify(message));
+  const message = {
+    type: 'join',
+    user: getUser(),
+  };
+  ws?.send(JSON.stringify(message));
 }
 
 function connect() {
-    if (ws !== null) {
-        console.warn('You are already connected!');
-        return;
+  if (ws !== null) {
+    console.warn('You are already connected!');
+    return;
+  }
+
+  ws = new WebSocket(WS_ENDPOINT);
+
+  ws.onopen = function () {
+    console.log('onConnectionOpen');
+    onConnectionOpen?.();
+    sendJoin();
+  };
+  ws.onerror = function () {
+    console.log('onConnectionError');
+    onConnectionError?.();
+  };
+  ws.onclose = function () {
+    ws = null;
+    console.log('onConnectionClose');
+    onConnectionClose?.();
+    setTimeout(() => {
+      connect();
+    }, 500);
+  };
+
+  ws.onmessage = function (event) {
+    /** @type {Message} */
+    const message = JSON.parse(event.data);
+    console.log('on' + message.type, message);
+    switch (message.type) {
+      case 'join':
+        onJoin(message);
+        break;
+      case 'leave':
+        onLeave(message);
+        break;
+      case 'message':
+        onReceiveMessage(message);
+        break;
+      case 'ban':
+        onBan(message);
+        break;
+      default:
+        console.warn('Receive unknown message', message);
     }
-
-    ws = new WebSocket(WS_ENDPOINT);
-
-    ws.onopen = function () {
-        console.log('onConnectionOpen');
-        onConnectionOpen?.();
-        sendJoin();
-    };
-    ws.onerror = function () {
-        console.log('onConnectionError');
-        onConnectionError?.();
-    };
-    ws.onclose = function () {
-        ws = null;
-        console.log('onConnectionClose');
-        onConnectionClose?.();
-        setTimeout(() => {
-            connect();
-        }, 500);
-    };
-
-    ws.onmessage = function (event) {
-        /** @type {Message} */
-        const message = JSON.parse(event.data);
-        console.log('on' + message.type, message);
-        switch (message.type) {
-            case 'join':
-                onJoin(message);
-                break;
-            case 'leave':
-                onLeave(message);
-                break;
-            case 'message':
-                onReceiveMessage(message);
-                break;
-            case 'ban':
-                onBan(message);
-                break;
-            default:
-                console.warn('Receive unknown message', message);
-        }
-    }
+  };
 }
